52a8ab57d733123e685c0e40890d4450c2167a68
GreenArmy <GreenArmyOne@gmail.com>
2024-12-11 18:48:14 +1000

    new OTA fix

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -5,5 +5,8 @@ const telegramUrl = 'https://t.me/UltimateCopyManager';
 const otaVersionUrl =
     "https://api.github.com/repos/BooksFine/re_ucm/releases/latest";
 const otaHost =
-    "https://github.com/BooksFine/re_ucm/releases/latest/download/re_ucm.apk";
+    "https://github.com/BooksFine/re_ucm/releases/latest/download/ReUCM_android.apk";
+// const windowsOTAHost =
+//     "https://github.com/BooksFine/re_ucm/releases/latest/download/ReUCM_windows.exe";
+
 const releasesUrl = "https://github.com/BooksFine/re_ucm/releases";

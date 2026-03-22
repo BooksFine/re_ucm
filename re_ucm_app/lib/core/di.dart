afc23bad7f0e8aab9cfa662793153f2bd0e9224f
InvisibleRun <invisiblerunbot@proton.me>
2026-03-22 14:52:21 +1000

    оптимизация инициализации бд

--- re_ucm_app/lib/core/di.dart
+++ re_ucm_app/lib/core/di.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:re_ucm_author_today/re_ucm_author_today.dart';
 import 'package:re_ucm_lib/re_ucm_lib.dart';
+import 'package:sembast/sembast_io.dart';
 
 import '../features/ota/ota_service.dart';
 
@@ -29,12 +30,16 @@ class AppDependencies extends InheritedWidget {
   bool updateShouldNotify(AppDependencies oldWidget) => false;
 
   static Future<AppDependencies> init({required Widget child}) async {
+    disableSembastCooperator();
+
     PortalFactory.registerAll([AuthorToday()]);
 
     final otaService = await OTAService.init();
     final dir = await getApplicationSupportDirectory();
     final recentBooksService = await RecentBooksService.init(dir.path);
     final settingsService = await SettingsService.init(dir.path);
+
+    enableSembastCooperator();
 
     return AppDependencies(
       otaService: otaService,
